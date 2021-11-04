cb04be3758f79872e2d45d59cbfc42e8c11d239a
guchen11 <82092769+guchen11@users.noreply.github.com>
2021-11-04 17:29:46 +0200

    collect data script

---
+++ collect_storage_data.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+storage_logs_home=/home/kni/storage_log
+number_of_collections=30
+sleep_intervals=10
+
+log_date=$(date +%m-%d-%Y)
+log_path=$storage_logs_home/storage_log_$log_date
+
+TOOLS_POD=$(oc get pods -n openshift-storage -l app=rook-ceph-tools -o name)
+
+result=$(oc rsh -n openshift-storage $TOOLS_POD ceph health)
+substr="${result:0:9}"
+
+if [[ "$substr" != "HEALTH_OK" ]]; then
+
+   FILE=$storage_logs_home
+
+   if [ ! -d "$FILE" ]; then
+       mkdir $FILE
+   fi
+
+   FILE=$log_path
+
+   #Only if new error at this date occurred collect logs
+   if [ ! -d "$FILE" ]; then
+
+        mkdir $FILE
+
+        for (( index=1; index<=$number_of_collections; index+=1 ))
+        do
+                TOOLS_POD=$(oc get pods -n openshift-storage -l app=rook-ceph-tools -o name)
+                echo `date` >> $log_path/ceph_summary.log
+                oc rsh -n openshift-storage $TOOLS_POD ceph -s >> $log_path/ceph_summary.log
+
+                TOOLS_POD=$(oc get pods -n openshift-storage -l app=rook-ceph-tools -o name)
+                echo `date` >> $log_path/ceph_osd_tree.log
+                oc rsh -n openshift-storage $TOOLS_POD ceph osd tree >> $log_path/ceph_osd_tree.log
+
+                TOOLS_POD=$(oc get pods -n openshift-storage -l app=rook-ceph-tools -o name)
+                echo `date` >> $log_path/ceph_df_detail.log
+                oc rsh -n openshift-storage $TOOLS_POD ceph df detail >> $log_path/ceph_df_detail.log
+
+                TOOLS_POD=$(oc get pods -n openshift-storage -l app=rook-ceph-tools -o name)
+                echo `date` >> $log_path/ceph_health_detail.log
+                oc rsh -n openshift-storage $TOOLS_POD ceph health detail >> $log_path/ceph_health_detail.log
+
+                sleep $sleep_intervals
+       done
+
+   fi
+
+fi
